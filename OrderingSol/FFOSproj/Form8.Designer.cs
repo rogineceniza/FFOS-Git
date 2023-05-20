faab4a2eed59a39d55419e331b4c5897573a55b5
Rogine Ceniza <rogineceniza0770@outlook.com>
2023-05-20 11:22:38 +0800

    Cashier form still under construction

---
+++ OrderingSol/FFOSproj/Form8.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace FFOSproj
+{
+    partial class loginForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panelForLoginAdminCashier = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panelForLoginAdminCashier.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panelForLoginAdminCashier
+            // 
+            this.panelForLoginAdminCashier.Controls.Add(this.label1);
+            this.panelForLoginAdminCashier.Controls.Add(this.pictureBox1);
+            this.panelForLoginAdminCashier.Location = new System.Drawing.Point(391, 22);
+            this.panelForLoginAdminCashier.Name = "panelForLoginAdminCashier";
+            this.panelForLoginAdminCashier.Size = new System.Drawing.Size(444, 473);
+            this.panelForLoginAdminCashier.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(39, 20);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(176, 129);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(115, 213);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // loginForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(860, 521);
+            this.Controls.Add(this.panelForLoginAdminCashier);
+            this.Name = "loginForm";
+            this.Text = "Login";
+            this.panelForLoginAdminCashier.ResumeLayout(false);
+            this.panelForLoginAdminCashier.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelForLoginAdminCashier;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
